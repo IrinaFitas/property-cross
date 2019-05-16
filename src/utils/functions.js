648163cf2fd5d1fff970b240f9f76cc0cc977810
Irina <Irinafitas07@gmail.com>
2019-05-16 15:12:45 +0300

    Rewrite pick function

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,10 +1,15 @@
-export const pick = function (obj, props) {
-    if (!obj || !props) return;
-    let picked = {};
+// export const pick = function (obj, props) {
+//     if (!obj || !props) return;
+//     let picked = {};
     
-	props.forEach(function(prop) {
-		picked[prop] = obj[prop];
-    });
+// 	props.forEach(function(prop) {
+// 		picked[prop] = obj[prop];
+//     });
     
-	return picked;
-};
+// 	return picked;
+// };
+
+export const pick = (propertyArr, obj) => {
+    return propertyArr.reduce((acc, elem) =>
+      (acc && acc[elem]) ? acc[elem] : null, obj);
+}
