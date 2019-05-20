6a4498d9971dd7b61e0aebce9861c05231e3b6b6
Irina <Irinafitas07@gmail.com>
2019-05-20 23:03:28 +0300

    Added ant-design library

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Antd from "ant-design-vue";
 import VueRouter from "vue-router";
 import { store } from "./store/store.js"; 
 import { routes } from "./routes.js";
+import "ant-design-vue/dist/antd.css";
 Vue.config.productionTip = false;
+
+Vue.use(Antd);
 
 Vue.use(VueRouter);
 const router = new VueRouter({
