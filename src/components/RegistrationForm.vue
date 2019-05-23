f3e9bc5cd1b137caaaa5efcab3d56b5653261067
Irina <Irinafitas07@gmail.com>
2019-05-23 12:59:39 +0300

    Wrote part of validate function

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,28 +1,37 @@
 <template>
     <div>
         <h3>Form Registration:</h3>
-        <a-form class="form">
+        <a-form class="form" @submit="validateAndSend" method="POST" novalidate="true">
             <a-form-item label="First Name:" :label-col="labelCol" :wrapper-col="wrapperCol">
-                <a-input placeholder="first name" />
-            </a-form-item>
-            
+                <a-input placeholder="first name" v-model="user.name" />
+            </a-form-item>            
 
             <a-form-item label="Last Name:" :label-col="labelCol" :wrapper-col="wrapperCol">
-                <a-input placeholder="last name" />
+                <a-input placeholder="last name" v-model="user.lastName" />
+            </a-form-item>
+
+            <a-form-item label="Email:" :label-col="labelCol" :wrapper-col="wrapperCol">
+                <a-input placeholder="email" type="email" v-model="user.email" />
             </a-form-item>
 
             <a-form-item label="Password:" :label-col="labelCol" :wrapper-col="wrapperCol">
-                <a-input placeholder="password" type="password" />
+                <a-input placeholder="password" type="password" v-model="user.password" />
             </a-form-item>
 
             <a-form-item label="Confirm password:" :label-col="labelCol" :wrapper-col="wrapperCol">
-                <a-input placeholder="password" type="password" />
+                <a-input placeholder="password" type="password" v-model="user.confirmPassword" />
             </a-form-item>
 
             <a-form-item>
                 <a-button type="primary" html-type="submit">Register</a-button>
             </a-form-item>
         </a-form>
+        <p>{{ user.name }}</p>
+        <p>{{ user.lastName}}</p>
+        <p>{{ user.email}}</p>
+        <p>{{ user.password }}</p>
+        <p>{{ user.confirmPassword }}</p>
+        <p v-for="(error, index) in errors" :key="index">{{ error }}</p>
     </div>
 </template>
 
@@ -38,7 +47,39 @@ export default {
                 xs: { span: 34 },
                 sm: { span: 12 },
             },
+            user: {
+                name: "",
+                lastName: "",
+                email: "",
+                password: "",
+                confirmPassword: ""
+            },
+            errors: []
         };
+    },
+    methods: {
+        validateAndSend(e) {
+            e.preventDefault();
+            if (!this.user.name) {
+                this.errors.push("Input your name, please");
+            }
+
+            if (!this.user.lastName) {
+                this.errors.push("Input your last name, please");
+            }
+
+            if (!this.user.email) {
+                this.errors.push("Input your email, please");
+            }
+
+            if (!this.user.password) {
+                this.errors.push("Input your  password, please");
+            }
+
+            if (!this.user.confirmPassword) {
+                this.errors.push("Input your  confirmPassword, please");
+            }
+        }
     }
 }
 </script>
@@ -53,8 +94,9 @@ export default {
     .form {
         width: 50%;
         padding: 15px;
-        border-radius: 5px;
-        border: 1px solid grey;
+        border-radius: 7px;
+        border: 1px solid var(--main-color);
+        box-shadow: 3px 2px 3px 0 var(--main-color);
         margin: 0 auto;
     }
 </style>
